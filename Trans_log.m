5336172be152531aa9ff76b7183b953438e1c050
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-10-23 18:01:08 -0500

    Optimisar

--- Trans_log.m
+++ Trans_log.m
@@ -4,7 +4,31 @@ close all;
 
 %Extraccion de las imagenes
 img1=imread('IMG\1.jpg');
-img2=imread('IMG\2.jpg');
-img3=imread('IMG\3.jpg');
-img4=imread('IMG\4.jpg');
+% img2=imread('IMG\2.jpg');
+% img3=imread('IMG\3.jpg');
+% img4=imread('IMG\4.jpg');
 
+%Interfaz para pedir el valor de C
+prompt="Ingrese el calor de C";
+t='Valor de C';
+dims=[1 50];
+definput={'10'};
+c=inputdlg(prompt,t,dims,definput);
+c=str2double(c);
+
+%Pasamos la img de uint8 a double
+i=double(img1);
+
+%Transformación logaritmica
+g=c*log(1+i);
+
+%Pasamos el resultado a uint8
+timg=uint8(g);
+
+figure()
+subplot(1,2,1)
+imshow(img1)
+title('Original')
+subplot(1,2,2)
+imshow(timg)
+title('Transformación logarítmica')
