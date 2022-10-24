d22d739af0934bf071ce1c17fa161474fcb391f3
R2 <83680549+ArturoEmmanuelToledoAguado@users.noreply.github.com>
2022-10-24 07:14:29 -0500

    Cambiar la img 3

--- Trans_log.m
+++ Trans_log.m
@@ -3,32 +3,34 @@ clear all;
 close all;
 
 %Extraccion de las imagenes
-img1=imread('IMG\1.jpg');
-% img2=imread('IMG\2.jpg');
-% img3=imread('IMG\3.jpg');
-% img4=imread('IMG\4.jpg');
+ruta={'IMG/1.jpg','IMG/2.jpg','IMG/3.jpg','IMG/4.jpg'};
 
-%Interfaz para pedir el valor de C
-prompt="Ingrese el calor de C";
-t='Valor de C';
-dims=[1 50];
-definput={'10'};
-c=inputdlg(prompt,t,dims,definput);
-c=str2double(c);
+for k=1:4
+    %Abrir imagen
+    img=imread(string(ruta(k)));
+    
+    %Interfaz para pedir el valor de C
+    prompt="Ingrese el calor de C";
+    t='Valor de C';
+    dims=[1 50];
+    definput={'10'};
+    c=inputdlg(prompt,t,dims,definput);
+    c=str2double(c);
 
-%Pasamos la img de uint8 a double
-i=double(img1);
+    %Pasamos la img de uint8 a double
+    i=double(img);
 
-%Transformación logaritmica
-g=c*log(1+i);
+    %Transformación logaritmica
+    g=c*log(1+i);
 
-%Pasamos el resultado a uint8
-timg=uint8(g);
+    %Pasamos el resultado a uint8
+    timg=uint8(g);
 
-figure()
-subplot(1,2,1)
-imshow(img1)
-title('Original')
-subplot(1,2,2)
-imshow(timg)
-title('Transformación logarítmica')
+    figure()
+    subplot(1,2,1)
+    imshow(img)
+    title('Original')
+    subplot(1,2,2)
+    imshow(timg)
+    title('Transformación logarítmica')
+end
